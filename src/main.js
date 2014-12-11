891840da6fc7f7f35b9669642cbbe2c806f6a7d0
Allen Evans <allen@blinkbox.com>
2014-12-11 06:32:57 +0000

    Define methods that will be exported.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,40 @@
+/*
+    main.js
+
+    Main entry point for essencejs library.
+ */
+
+/**
+ * Register an object by a given injectable name.
+ */
+function register() {
+
+}
+
+/**
+ * Register a single instance (singleton) of an object that can be instantiated.
+ */
+function singleton() {
+
+}
+
+/**
+ * Register a factory to create new instances of an object that can be instantiated.
+ */
+function factory() {
+
+}
+
+/**
+ * Take a given function and as soon as all its parameters can be resolved, execute that function.
+ */
+function inject() {
+
+}
+
 module.exports = {
-    // Todo.
+    register : register,
+    singleton : singleton,
+    factory : factory,
+    inject : inject
 };
