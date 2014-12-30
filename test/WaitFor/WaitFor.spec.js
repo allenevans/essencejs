eef7fc1f9b94b079783b0ce78da81f4a186d2c36
Allen Evans <allenevans@hotmail.com>
2014-12-30 10:04:21 +0000

    Additional test for WaitFor to improve code coverage metrics.

--- test/WaitFor/WaitFor.spec.js
+++ test/WaitFor/WaitFor.spec.js
@@ -9,12 +9,30 @@ var path = require("path");
 module.exports = {
     "WaitFor should be exported":
         function (test) {
-            var WaitFor = require(path.join(process.cwd(), "src/WaitFor.js"));
-
             test.expect(2);
+
+            var WaitFor = require(path.join(process.cwd(), "src/WaitFor.js"));
 
             test.equal(!!WaitFor, true, "Expected to be able to require WaitFor");
             test.equal(WaitFor.name, "WaitFor", "Expected name to be `WaitFor`");
+
+            test.done();
+        },
+    "WaitFor callback should always be set to a function":
+        function (test) {
+            test.expect(4);
+
+            var WaitFor = require(path.join(process.cwd(), "src/WaitFor.js"));
+
+            var waitFor1 = new WaitFor(),
+                waitFor2 = new WaitFor({
+                    callback : null
+                });
+
+            test.equal(typeof waitFor1.callback === "function", true);
+            test.equal(typeof waitFor2.callback === "function", true);
+            test.equal(waitFor1.callback(), undefined);
+            test.equal(waitFor2.callback(), undefined);
 
             test.done();
         }
